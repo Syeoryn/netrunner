9dce4ddd56da2c0bf0cd1672cee0020ed5e7dcc4
Drew Cuthbertson <drew.cuthbertson@airbnb.com>
2023-07-01 00:28:30 -0700

    Update winning corp and runner

--- netrunner/app.py
+++ netrunner/app.py
@@ -1,12 +1,21 @@
-import streamlit as st
+import base64
+import textwrap
 
+import streamlit as st
 from clients.abr_client import AbrClient
 from clients.nrdb_client import NrdbClient
+from data import mappings
 
 st.set_page_config(page_title="Netrunner", page_icon="assets/NSG-Visual-Assets/SVG/Game Symbols/NISEI_AGENDA.png")
 
 abr_client = AbrClient()
 nrdb_client = NrdbClient()
+
+def get_faction_glyph(faction_code):
+    mapping = mappings.FACTION_ASSETS.get(faction_code)
+    if mapping is not None:
+        return mappings.BASE_FACTION_GLYPH_PATH.format(mapping["glyph_path"])
+
 
 def render_tournament(tournament):
     "Event: " + tournament["title"]
@@ -14,12 +23,15 @@ def render_tournament(tournament):
     "Date: " + tournament["date"]
     "Format: " + tournament["format"]
     "Cardpool: " + tournament["cardpool"]
-    runner = tournament["winner_runner_identity"]
-    corp = tournament["winner_corp_identity"]
-    st.write(runner)
-    st.write(nrdb_client.get_card(runner[0]))
-    # "Winner Runner: " + nrdb_client.get_card(runner)
-    # "Winner Corp: " + nrdb_client.get_card(corp)
+
+    runner_id = tournament["winner_runner_identity"]
+    runner = nrdb_client.get_card(runner_id)["data"][0]
+    st.write("Winner Runner: {}  ({})".format(runner["title"], runner["faction_code"]))
+
+    corp_id = tournament["winner_corp_identity"]
+    corp = nrdb_client.get_card(corp_id)["data"][0]
+    st.write("Winner Corp: {}  ({})".format(corp["title"], corp["faction_code"]))
+
 
 "### Last Tournament"
 last_tournament = abr_client.get_tournament_results(1)[0]
